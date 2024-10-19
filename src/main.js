6acdfe3dd649a8abec3148d8fee9d4c3f4682925
Karen Dantas <karennobre74@gmail.com>
2024-10-19 12:06:22 -0300

    feat: finish main container styles

---
+++ src/main.js
@@ -0,0 +1,58 @@
+products = [
+    {
+        id: 1,
+        title: 'Blusa denim tubo',
+        price: 49.99,
+        image: './assets/img/blusa-denim.png',
+        stock: true
+    },
+    {
+        id: 2,
+        title: 'Saia denim plissado',
+        price: 59.99,
+        image: './assets/img/saia-denim.png',
+        stock: true
+    },
+    {
+        id: 3,
+        title: 'Blusa top denim curto',
+        price: 49.99,
+        image: './assets/img/blusa-top.png',
+        stock: true
+    },
+    {
+        id: 4,
+        title: 'Blusa top denim turbo',
+        price: 49.99,
+        image: './assets/img/blusa-turbo.png',
+        stock: true
+    }, 
+    {
+        id: 5,
+        title: 'Short-saia denim rodado',
+        price: 49.99,
+        image: './assets/img/short-saia.png',
+        stock: true
+    }, 
+    {
+        id: 6,
+        title: 'Vestido denim rodado',
+        price: 69.99,
+        image: './assets/img/vestido.png',
+        stock: true
+    },
+    {
+        id: 7,
+        title: 'Blusa top denim coração',
+        price: 49.99,
+        image: './assets/img/blusa-denim2.png',
+        stock: true
+    },
+    {
+        id: 8,
+        title: 'Saia denim plissado jeans',
+        image: './assets/img/saia-denim2.png',
+        stock: false
+
+    }
+]
