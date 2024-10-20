b21a8958276ddf8e96f547989fc51a38c4deb47e
Karen Dantas <karennobre74@gmail.com>
2024-10-20 20:42:45 -0300

    feat: add dynamic products with js

--- src/main.js
+++ src/main.js
@@ -1,58 +1,64 @@
-products = [
-    {
-        id: 1,
-        title: 'Blusa denim tubo',
-        price: 49.99,
-        image: './assets/img/blusa-denim.png',
-        stock: true
-    },
-    {
-        id: 2,
-        title: 'Saia denim plissado',
-        price: 59.99,
-        image: './assets/img/saia-denim.png',
-        stock: true
-    },
-    {
-        id: 3,
-        title: 'Blusa top denim curto',
-        price: 49.99,
-        image: './assets/img/blusa-top.png',
-        stock: true
-    },
-    {
-        id: 4,
-        title: 'Blusa top denim turbo',
-        price: 49.99,
-        image: './assets/img/blusa-turbo.png',
-        stock: true
-    }, 
-    {
-        id: 5,
-        title: 'Short-saia denim rodado',
-        price: 49.99,
-        image: './assets/img/short-saia.png',
-        stock: true
-    }, 
-    {
-        id: 6,
-        title: 'Vestido denim rodado',
-        price: 69.99,
-        image: './assets/img/vestido.png',
-        stock: true
-    },
-    {
-        id: 7,
-        title: 'Blusa top denim coração',
-        price: 49.99,
-        image: './assets/img/blusa-denim2.png',
-        stock: true
-    },
-    {
-        id: 8,
-        title: 'Saia denim plissado jeans',
-        image: './assets/img/saia-denim2.png',
-        stock: false
+import { products } from "./utils/products";
 
+const productsContainer = document.querySelector('.products-container');
+
+console.log(productsContainer);
+productsContainer.innerHTML = null;
+products.forEach((product) => {
+
+
+    const divCard = document.createElement('div');
+    divCard.classList.add('card')
+
+    const imgCard = document.createElement('img');
+    imgCard.src = product.image;
+    divCard.appendChild(imgCard);
+
+    const releaseDiv = document.createElement('div');
+    releaseDiv.classList.add('release-tag')
+    releaseDiv.textContent = 'lançamento';
+    divCard.appendChild(releaseDiv);
+
+    const highlightDiv = document.createElement('div');
+    highlightDiv.classList.add('product-highlight');
+
+    const titleH3 = document.createElement('h3');
+    titleH3.classList.add('beige-text');
+    titleH3.textContent = product.title;
+    highlightDiv.appendChild(titleH3);
+    
+
+    const priceDiv = document.createElement('div');
+    const priceSpan = document.createElement('span');
+    const priceSpan2 = document.createElement('span');
+    priceSpan.classList.add('blue-text');
+    priceSpan2.classList.add('blue-text');
+
+    if (product.stock === true){
+
+        priceSpan.classList.add('price');
+        priceSpan2.classList.add('alt-price');
+    
+        priceSpan.textContent = `R$ ${product.price}`;
+        priceSpan2.textContent = `R$ ${product.price} à vista com desconto pix`;
+        priceDiv.appendChild(priceSpan);
+        priceDiv.appendChild(priceSpan2);
+    
+        highlightDiv.appendChild(priceDiv);
+    }else{
+        priceDiv.classList.add('out-stock');
+        priceSpan.classList.add('price');
+        
+        priceSpan.textContent = 'Esse acabou :(';
+        priceSpan2.textContent = 'Avise-me quando chegar';
+        priceDiv.appendChild(priceSpan);
+        priceDiv.appendChild(priceSpan2);
+    
+        highlightDiv.appendChild(priceDiv);
     }
-]
+
+    divCard.appendChild(highlightDiv);
+
+    productsContainer.appendChild(divCard);
+
+})
